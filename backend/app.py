5ff070af9b4aee0276a88b5c38493f835583388d
Jayesh Narwaria <36211228+jaynarw@users.noreply.github.com>
2021-05-31 23:00:56 +0530

    Change fetch to realtime db

--- backend/app.py
+++ backend/app.py
@@ -2,32 +2,40 @@ from logging import debug
 from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import db
 import json
 from flask_cors import CORS, cross_origin
+
 """
     Initialize firebase app
 """
-cred = credentials.Certificate('exam-vault-firebase-adminsdk-he4z9-a435dd6534.json')
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+cred = credentials.Certificate("exam-vault-firebase-adminsdk-he4z9-a435dd6534.json")
+firebase_admin.initialize_app(
+    cred,
+    {
+        "databaseURL": "https://exam-vault-default-rtdb.firebaseio.com/",
+    },
+)
+
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-@app.route('/',methods=['GET'])
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+@app.route("/", methods=["GET"])
 def home():
-    msg = 'Backend for Exam_Vault. ADMIN ACCESS ONLY'
+    msg = "Backend for Exam_Vault. ADMIN ACCESS ONLY"
     return msg
-@app.route('/data',methods=['POST','GET'])
+
+
+@app.route("/data", methods=["POST", "GET"])
 @cross_origin()
 def get_data_from_firebase():
-    data_list = []
-    
-    docs = db.collection(u'covid').stream()
-    for doc in docs:
+    ref = db.reference(
+        "1gnOVIlv3xDotqqsDeFXRoFF4rkGCVYgEwQ3CH0iPlYo/Form%20responses%201/"
+    )
+    return json.dumps(ref.get())
 
-        data_list.append(({"Course":doc.get('state'),"Link":doc.get('cases')}))
-    data = {"uploaded_files":data_list}
-    return json.dumps(data)
-if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
