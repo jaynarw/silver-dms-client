02a1142c9a9bd54793e4fee57f9cbb3c32629beb
Jayesh Narwaria <36211228+jaynarw@users.noreply.github.com>
2021-05-31 23:30:19 +0530

    Update FileViewer to Functional Component

--- src/updComponents/home/FileViewer.js
+++ src/updComponents/home/FileViewer.js
@@ -1,92 +1,66 @@
-import React from 'react';
-import {Table, Space, Button} from "antd";
+import React, { useState, useEffect } from 'react';
+import {Table, Space, Button, Spin} from "antd";
 import "antd/dist/antd.css";
-class FileContainer extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            fileArray:[
-                {
 
-                    providerName:'Harshit',
-                    course:'MTH101',
-                    files:['file1','file2'],
-                    editing:false,
-                    
-                },
-                {
+function FileContainer() {
+    const [loading, setLoading] = useState(true);
+    const [fileArray, setFileArray] = useState([]);
 
-                    providerName:'Harshit',
-                    course:'MTH101',
-                    files:['file1','file2'],
-                    editing:false,
-                },
-                {
-
-                    providerName:'Harshit',
-                    course:'MTH101',
-                    files:['file1','file2'],
-                    editing:false,
-                },
-                {
-
-                    providerName:'Harshit',
-                    course:'MTH101',
-                    files:['file1','file2'],
-                    editing:false,
-                },
-
-            ],
+    useEffect(() => {
+        fetch('http://localhost:5000/data').then((res) => res.json()).then((files) => {
+            console.log(files);
+            setFileArray(files);
+            setLoading(false);
+        });
+        return () => {
+            console.log("This will be called when unmounted");
         };
-    }
-    render(){
-        const columns=[
-            {
-                title: 'Uploaded By',
-                dataIndex:'providerName',
-                key:'name',
+    }, []);
     
-            },
-            {
-                title: 'Course',
-                dataIndex:'course',
-                key:'course',
-            },
-            {
-                title:'Uploaded Files',
-                dataIndex:'files',
-                key:'files',
-                render:(files) => files.map((file, index)=>
-                    <li><a href={file}>View File {index}</a></li>
-                ),
-            },
-            {
-                title:'Edit File',
-                dataIndex:'edit-file',
-                key:'edit-file',
-                render:()=>(
-                    <Button type="primary">Rename</Button>
-                ),
-            },
-            {
-                title:'Actions',
-                dataIndex:'actions',
-                key:'actions',
-                render:()=>(
-                    <Space size="small">
-                        <Button type="link" >Accept</Button>
-                        <Button type="link">Reject</Button>
-                    </Space>
-                ),
-            
-            },
-        ];
+    const columns=[
+        {
+            title: 'Uploaded By',
+            dataIndex:'providerName',
+            key:'name',
 
-
-
-        return(
-            <Table columns={columns} dataSource={this.state.fileArray}/>
-        );
-    }
+        },
+        {
+            title: 'Course',
+            dataIndex:'course',
+            key:'course',
+        },
+        {
+            title:'Uploaded Files',
+            dataIndex:'files',
+            key:'files',
+            render:(files) => files.map((file, index)=>
+                <li><a href={file}>View File {index}</a></li>
+            ),
+        },
+        {
+            title:'Edit File',
+            dataIndex:'edit-file',
+            key:'edit-file',
+            render:()=>(
+                <Button type="primary">Rename</Button>
+            ),
+        },
+        {
+            title:'Actions',
+            dataIndex:'actions',
+            key:'actions',
+            render:()=>(
+                <Space size="small">
+                    <Button type="link" >Accept</Button>
+                    <Button type="link">Reject</Button>
+                </Space>
+            ),
+        
+        },
+    ];
+    return (<>
+    {loading && <Spin spinning />}
+    {!loading && <Table columns={columns} dataSource={fileArray}/>}
+    </>);
 }
 export default FileContainer;
